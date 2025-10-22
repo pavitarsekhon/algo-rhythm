2a2ef129d4c0d0fd6aea2e80e06cb8abee912b51
Pavitar Sekhon <pavitar.sekhon2003@hotmail.com>
2025-10-22 16:48:46 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/algorhythm/AlgorhythmApplication.kt
@@ -0,0 +1,11 @@
+package com.example.algorhythm
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlgorhythmApplication
+
+fun main(args: Array<String>) {
+	runApplication<AlgorhythmApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/algorhythm/AlgorhythmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.algorhythm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlgorhythmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
