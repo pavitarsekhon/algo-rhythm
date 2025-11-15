af3a75ce72be771f98907d493fa73dd131374af1
Pavitar Sekhon <pavitar.sekhon2003@hotmail.com>
2025-11-15 22:09:02 +0000

    update: cleaned question controller

--- src/main/kotlin/com/example/algorhythm/api/controller/QuestionController.kt
+++ src/main/kotlin/com/example/algorhythm/api/controller/QuestionController.kt
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.example.algorhythm.api.domain.Question
+import com.example.algorhythm.api.repository.UserSessionRepository
+import com.example.algorhythm.api.service.Judge0ResultResponse
 import com.example.algorhythm.api.service.SubmitResultResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -17,28 +19,37 @@ import org.springframework.web.bind.annotation.RequestBody
 class QuestionController (
     private val questionRepository: QuestionRepository,
     private val judge0Service: Judge0Service,
+    private val userSessionRepository: UserSessionRepository
 ) {
 
     @GetMapping("/next")
     fun getNextQuestion(): Question? {
+        val userSession = userSessionRepository.findByActiveIsTrue()
+        val userCurrentQuestionId = userSession.currentQuestionId ?: error("TODO: Empty question id - give user question based on difficulty")
         // clear code window
         // random difficulty for mvp - will add adaptive difficulty later on
-        val difficulty = QuestionDifficulty.entries.random()
-        val questions = questionRepository.findByDifficulty(difficulty)
+        val question = questionRepository.findById(userCurrentQuestionId)
+            .orElseThrow { IllegalArgumentException("Question not found") }
 
-        if (questions.isEmpty()) {
-            error("No entries were extracted from db")
-        }
-        return questions.first()
+        return question
     }
 
     @PostMapping("/submit")
     fun submitCode(@RequestBody request: CodeSubmissionRequest): SubmitResultResponse = judge0Service.submitCode(request)
+
+    @PostMapping("/run")
+    fun runCode(@RequestBody request: RunCodeRequest): Judge0ResultResponse = judge0Service.runCode(request.code, request.language, request.input)
 
     data class CodeSubmissionRequest(
         val code: String,
         val language: String,
         val input: String? = null,
         val questionId: Long
+    )
+
+    data class RunCodeRequest(
+        val code: String,
+        val language: String,
+        val input: String? = null
     )
 }
